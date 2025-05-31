f139ee3a996b8af0c4f2a3369795d1c0e153d4ca
aidoyuzuna <hoshipaso+gi@gmail.com>
2025-05-31 22:38:23 +0900

    👍 config.jsなど設定関係ファイルを追加

---
+++ vivliostyle.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "Vivliostyle SandBox",
+  author: "相戸ゆづな",
+  language: "ja",
+  size: "A5",
+  theme: "css/content-style.css",
+  entry: [
+    "docs/section/sc-chapter1.md",
+    "docs/content/chapter1.md",
+  ],
+  output: ["output.pdf"]
+};
